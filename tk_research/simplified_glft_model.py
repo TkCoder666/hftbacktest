db3a1163922d4ed1b2b0477b0370abb3305141aa
tangke <tangke@qsqcapital.com>
2025-01-01 18:00:20 +0800

    only reset order when change exceed reset_bps

--- tk_research/simplified_glft_model.py
+++ tk_research/simplified_glft_model.py
@@ -67,24 +67,14 @@ def linear_regression(x, y):
     return slope, intercept
 
 
-out_dtype = np.dtype([
-    ('cur_ts', '<i8'),
-    ('mid', 'f8'),
-    ('half_spread_tick', 'f8'),
-    ('bp0', 'f8'),
-    ('ap0', 'f8'),
-    ('bid_price', 'f8'),
-    ('ask_price', 'f8'),
-    ('bid_usd_barrier', 'f8'),
-    ('ask_usd_barrier', 'f8'),
-])
 
 @njit
-def gridtrading_glft_mm(hbt : ROIVectorMarketDepthBacktest, recorder, gamma, order_qty):
+def gridtrading_glft_mm(hbt, recorder, gamma, order_qty):
     asset_no = 0
     tick_size = hbt.depth(asset_no).tick_size
-    arrival_depth = np.full(buffer_size, np.nan, np.float64)
-    mid_price_chg = np.full(buffer_size, np.nan, np.float64)
+
+    arrival_depth = np.full(700_000_000, np.nan, np.float64)
+    mid_price_chg = np.full(700_000_000, np.nan, np.float64)
 
     t = 0
     prev_mid_price_tick = np.nan
@@ -100,7 +90,12 @@ def gridtrading_glft_mm(hbt : ROIVectorMarketDepthBacktest, recorder, gamma, ord
 
     grid_num = 20
     max_position = 50 * order_qty
-    out = np.zeros(buffer_size, out_dtype)
+    
+    last_lvl1_bid = None
+    last_lvl1_ask = None
+    reset_bps = 10
+    reset_cnt = 0
+
     # Checks every 100 milliseconds.
     while hbt.elapse(100_000_000) == 0:
         #--------------------------------------------------------
@@ -167,8 +162,7 @@ def gridtrading_glft_mm(hbt : ROIVectorMarketDepthBacktest, recorder, gamma, ord
 
         bid_price_tick = min(np.round(reservation_price_tick - half_spread_tick), best_bid_tick)
         ask_price_tick = max(np.round(reservation_price_tick + half_spread_tick), best_ask_tick)
-        # make bid_price_tick from np.float64 to np.int64
-        
+
         bid_price = bid_price_tick * tick_size
         ask_price = ask_price_tick * tick_size
 
@@ -176,107 +170,72 @@ def gridtrading_glft_mm(hbt : ROIVectorMarketDepthBacktest, recorder, gamma, ord
 
         bid_price = np.floor(bid_price / grid_interval) * grid_interval
         ask_price = np.ceil(ask_price / grid_interval) * grid_interval
-        # calc usd value before placing orders
-        #! how to deal with invalid data
-        
-        if not np.isnan(bid_price) and not np.isnan(ask_price):
-            ask_usd_barrier = 0
-            bid_usd_barrier = 0
-            for tick_price in range(depth.best_ask_tick, ask_price_tick + 1):
-                qty = depth.ask_qty_at_tick(tick_price)
-                if qty > 0:
-                    ask_usd_barrier += qty * tick_price * tick_size
-            
-            for tick_price in range(depth.best_bid_tick, max(bid_price_tick,0) - 1, -1):
-                qty = depth.bid_qty_at_tick(tick_price)
-                if qty > 0:
-                    bid_usd_barrier += qty * tick_price * tick_size
-        else:
-            bid_usd_barrier = np.nan
-            ask_usd_barrier = np.nan
+
         #--------------------------------------------------------
         # Updates quotes.
 
         # Creates a new grid for buy orders.
-        new_bid_orders = Dict.empty(uint64, float64)
-        if position < max_position and np.isfinite(bid_price):
-            for i in range(grid_num):
-                bid_price_tick = round(bid_price / tick_size)
+        if (last_lvl1_bid is None or abs(last_lvl1_bid - bid_price) / last_lvl1_bid> reset_bps / 1e4) or (last_lvl1_ask is None or abs(last_lvl1_ask - ask_price) / last_lvl1_ask > reset_bps / 1e4):
+            last_lvl1_bid = bid_price
+            last_lvl1_ask = ask_price
+            reset_cnt += 1
+            order_values = orders.values()
+            while order_values.has_next():
+                order = order_values.get()
+                # Cancels if a working order is not in the new grid.
+                if order.cancellable:
+                    hbt.cancel(asset_no, order.order_id, False)
 
-                # order price in tick is used as order id.
-                new_bid_orders[uint64(bid_price_tick)] = bid_price
+            new_bid_orders = Dict.empty(uint64, float64)
+            if position < max_position and np.isfinite(bid_price):
+                for i in range(grid_num):
+                    bid_price_tick = round(bid_price / tick_size)
+                    # order price in tick is used as order id.
+                    new_bid_orders[uint64(bid_price_tick)] = bid_price
+                    bid_price -= grid_interval
 
-                bid_price -= grid_interval
-
-        # Creates a new grid for sell orders.
-        new_ask_orders = Dict.empty(uint64, float64)
-        if position > -max_position and np.isfinite(ask_price):
-            for i in range(grid_num):
-                ask_price_tick = round(ask_price / tick_size)
-
-                # order price in tick is used as order id.
-                new_ask_orders[uint64(ask_price_tick)] = ask_price
-
-                ask_price += grid_interval
-
-        # order_values = orders.values();
-        # while order_values.has_next():
-        #     order = order_values.get()
-        #     # Cancels if a working order is not in the new grid.
-        #     if order.cancellable:
-        #         if (
-        #             (order.side == BUY and order.order_id not in new_bid_orders)
-        #             or (order.side == SELL and order.order_id not in new_ask_orders)
-        #         ):
-        #             hbt.cancel(asset_no, order.order_id, False)
-
-        # for order_id, order_price in new_bid_orders.items():
-        #     # Posts a new buy order if there is no working order at the price on the new grid.
-        #     if order_id not in orders:
-        #         hbt.submit_buy_order(asset_no, order_id, order_price, order_qty, GTX, LIMIT, False)
-
-        # for order_id, order_price in new_ask_orders.items():
-        #     # Posts a new sell order if there is no working order at the price on the new grid.
-        #     if order_id not in orders:
-        #         hbt.submit_sell_order(asset_no, order_id, order_price, order_qty, GTX, LIMIT, False)
-
+            for order_id, order_price in new_bid_orders.items():
+                # Posts a new buy order if there is no working order at the price on the new grid.
+                if order_id not in orders:
+                    hbt.submit_buy_order(asset_no, order_id, order_price, order_qty, GTX, LIMIT, False)        
+                    
+            
+            new_ask_orders = Dict.empty(uint64, float64)
+            if position > -max_position and np.isfinite(ask_price):
+                for i in range(grid_num):
+                    ask_price_tick = round(ask_price / tick_size)
+                    # order price in tick is used as order id.
+                    new_ask_orders[uint64(ask_price_tick)] = ask_price
+                    ask_price += grid_interval
+                    
+            for order_id, order_price in new_ask_orders.items():
+                # Posts a new sell order if there is no working order at the price on the new grid.
+                if order_id not in orders:
+                    hbt.submit_sell_order(asset_no, order_id, order_price, order_qty, GTX, LIMIT, False)
         #--------------------------------------------------------
         # Records variables and stats for analysis.
-        out[t]['cur_ts'] = hbt.current_timestamp
-        out[t]['mid'] = mid_price_tick * tick_size
-        out[t]['half_spread_tick'] = half_spread_tick
-        out[t]['ap0'] = best_ask_tick * tick_size
-        out[t]['bp0'] = best_bid_tick * tick_size
-        out[t]['bid_price'] = bid_price
-        out[t]['ask_price'] = ask_price
-        out[t]['bid_usd_barrier'] = bid_usd_barrier
-        out[t]['ask_usd_barrier'] = ask_usd_barrier
-        
-        # Records the current state for stat calculation.
-        recorder.record(hbt)
-        
+
         t += 1
 
         if t >= len(arrival_depth) or t >= len(mid_price_chg):
             raise Exception
+
         # Records the current state for stat calculation.
         recorder.record(hbt)
-    return out[:t]
         
         
 
 from hftbacktest.stats import LinearAssetRecord
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
-import gc
+
 if __name__ == '__main__':
     # This backtest assumes market maker rebates.
     # https://www.binance.com/en/support/announcement/binance-upgrades-usd%E2%93%A2-margined-futures-liquidity-provider-program-2023-04-04-01007356e6514df3811b0c80ab8c83bf
     asset_name = "ETHUSDT"
     begin_date =  "20240320"
-    end_date =  "20240401"
+    end_date =  "20240604"
     data_dir =  "/mnt/data/hftbacktest_data/data"
-    out_dir =  "stats/order_stat"
     start = datetime.strptime(begin_date, "%Y%m%d")
     prev_date = (start - timedelta(days=1)).strftime("%Y%m%d")
     end = datetime.strptime(end_date, "%Y%m%d")
@@ -305,15 +264,18 @@ if __name__ == '__main__':
     )
     print("Start backtest.")
     t1 = time.time()
-    gamma_list = [0.5,1, 5]
-    buffer_size = 700_000_000
-    for gamma in gamma_list:
-        order_qty = 0.001
-        recorder = Recorder(1, buffer_size)
+    gamma = 5
+    order_qty_list = [0.1,1,10]
+    for order_qty in order_qty_list:
+        recorder = Recorder(1, 700_000_000)
         hbt = ROIVectorMarketDepthBacktest([asset])
-        out = gridtrading_glft_mm(hbt, recorder.recorder, gamma, order_qty)
+        gridtrading_glft_mm(hbt, recorder.recorder, gamma, order_qty)
         hbt.close()
-        # save out to npz 
-        np.savez(f"{out_dir}/simple_glft_mm_{gamma}.npz", out=out)
-        del out
-        gc.collect()
+        t2 = time.time()
+        print(f"Elapsed time: {t2 - t1:.2f} seconds for the backtest.")
+        recorder.to_npz(f'stats/gridtrading_simple_glft_mm1_{asset_name}_{gamma}_{order_qty}_{begin_date}_{end_date}.npz')
+        stats = LinearAssetRecord(recorder.get(0)).stats()
+        print(stats.summary())
+        stats.plot()
+        plt.savefig(f'stats/gridtrading_simple_glft_mm1_{asset_name}_{gamma}_{order_qty}_{begin_date}_{end_date}.png')
+        
